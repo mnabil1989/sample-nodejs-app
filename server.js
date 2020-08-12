9100f4d323da92e8f36e18cb7f51ec53fbf9aab6
mohamed_nabil20 <mohamed.elbohy@andalusiagroup.net>
2020-08-12 16:11:20 +0000

    eedffgv

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
     res.send('Hello world from a Node.js app! welcome to nabil profile Thanks for your interesting')
 })
 
-app.listen(4000, () => {
-    console.log('Server is up on 4000')
+app.listen(3000, () => {
+    console.log('Server is up on 3000')
 })
